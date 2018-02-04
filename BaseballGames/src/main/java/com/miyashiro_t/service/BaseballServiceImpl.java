0d83747fa6fd659b964aebbca36acf4eacb95fbd
i130622c <i130622c@gmail.com>
2018-02-04 20:48:09 +0900

    DB connection is success.

--- BaseballGames/src/main/java/com/miyashiro_t/service/BaseballServiceImpl.java
+++ BaseballGames/src/main/java/com/miyashiro_t/service/BaseballServiceImpl.java
@@ -1,21 +1,30 @@
 package com.miyashiro_t.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.miyashiro_t.dao.BaseballDao;
+import com.miyashiro_t.dao.DaoFactory;
 import com.miyashiro_t.vo.BaseballPlayerHdVo;
 
 @Component
 public class BaseballServiceImpl implements BaseballService{
 
+	private BaseballDao baseballDao;
+	
+	@Autowired
+	public BaseballServiceImpl(DaoFactory factory) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.baseballDao = factory.getBaseballDao();
+	}
+	
 	@Override
 	public List<BaseballPlayerHdVo> load() {
 		// TODO 自動生成されたメソッド・スタブ
-		List<BaseballPlayerHdVo> baseballPlayerHdVos = new ArrayList<>();
-		BaseballPlayerHdVo target = new BaseballPlayerHdVo.Builder().id(1).name_kanji("kanji").name_kana("kana").build();
-		baseballPlayerHdVos.add(target);
+		List<BaseballPlayerHdVo> baseballPlayerHdVos = baseballDao.load();
+//		BaseballPlayerHdVo target = new BaseballPlayerHdVo.Builder().id(1).name_kanji("kanji").name_kana("kana").build();
 		return baseballPlayerHdVos;
 	}
 
