bce1d69854c06e15d58e362cb241ef88caf10c4e
i130622c <i130622c@gmail.com>
2018-02-18 21:41:08 +0900

    thyemleaf is success.

--- BaseballGames/src/main/java/com/miyashiro_t/baseballGames/HomeController.java
+++ BaseballGames/src/main/java/com/miyashiro_t/baseballGames/HomeController.java
@@ -15,27 +15,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-@RequestMapping(value="/")
+@RequestMapping(value = "/")
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
+		// Date date = new Date();
+		// DateFormat dateFormat =
+		// DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG,
+		// locale);
+		//
+		// String formattedDate = dateFormat.format(date);
+		//
+		// model.addAttribute("serverTime", formattedDate );
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		String message = "  The time on the server is " + formattedDate + ". ";
+
+		model.addAttribute("hello", message);
+
 		return "home";
 	}
-	
-	
+
 }
